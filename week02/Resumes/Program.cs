8059e3f46ce98136a916016bccf4de8150720f14
Proffvintage <145012191+Proffvintage@users.noreply.github.com>
2025-09-28 16:17:05 +0100

    Update Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,28 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Secreteary";
+        job1._company = "October Very Own's Merch";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Auditor";
+        job2._company = "ANker Soundcore PLC";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Markson Samuel";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
