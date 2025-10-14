cf2c560080cdad80d487d92ecda8276d0daae958
Proffvintage <145012191+Proffvintage@users.noreply.github.com>
2025-10-14 08:58:30 +0100

    Create Customer.cs

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public string Name { get { return name; } }
+
+    public bool LivesInNgr()
+    {
+        return address.IsInNgr();
+    }
+
+    public string GetAddressString()
+    {
+        return address.GetFullAddress();
+    }
+}
