a315a7f08af4009948833aec1c4c9c085bec38b8
Proffvintage <145012191+Proffvintage@users.noreply.github.com>
2025-10-15 13:16:57 +0100

    Update Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+
+            new Cycling(new DateTime(2022, 11, 4), 40, 18.5),
+
+            new Swimming(new DateTime(2022, 11, 5), 25, 40)
+        };
+
+        foreach (var a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
