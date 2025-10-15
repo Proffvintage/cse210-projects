39c45b4a1f43bce55af8853c383e251d3ba8cacf
Proffvintage <145012191+Proffvintage@users.noreply.github.com>
2025-10-15 13:17:48 +0100

    Update Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -7,11 +7,11 @@ class Program
     {
         var activities = new List<Activity>
         {
-            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+            new Running(new DateTime(2024, 9, 3), 30, 3.0),
 
-            new Cycling(new DateTime(2022, 11, 4), 40, 18.5),
+            new Cycling(new DateTime(2024, 9, 4), 40, 18.5),
 
-            new Swimming(new DateTime(2022, 11, 5), 25, 40)
+            new Swimming(new DateTime(2024, 9, 5), 25, 40)
         };
 
         foreach (var a in activities)
